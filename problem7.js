a7068db92ce73ff4254f61a8e4d2cdf72c733259
SR Tonmoy <sahriarrahman701@gmail.com>
2026-01-08 17:58:16 +0600

    solve 8 and 7

---
+++ problem7.js
@@ -0,0 +1,9 @@
+function findEvenNumbers(arr) {
+  let evens = [];
+  for (let num of arr) {
+    if (num % 2 === 0) {
+      evens.push(num);
+    }
+  }
+  return evens;
+}
---
+++ problem8.js
@@ -0,0 +1,10 @@
+function capitalizeWords(str) {
+  let words = str.split(" ");
+  let result = [];
+
+  for (let word of words) {
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return result.join(" ");
+}
